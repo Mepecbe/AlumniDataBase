86af704589eac59809d440f008fb4925d3b4c815
Никита <val0dko99@mail.ru>
2020-10-08 20:51:31 +0300

    Остальное

--- Form1.cs
+++ Form1.cs
@@ -1,12 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
+using System.Xml;
 using System.Data;
-using System.Drawing;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Drawing;
 using System.Windows.Forms;
+using System.ComponentModel;
+using System.Threading.Tasks;
+using System.Collections.Generic;
 
 using MetroFramework;
 
@@ -17,6 +18,51 @@ namespace DataBase
         public Form1()
         {
             InitializeComponent();
+        }
+    }
+
+
+
+
+    /// <summary>
+    /// Ведомость персонального учета выпускников
+    /// </summary>
+    public class Statement1
+    {
+        public readonly string EducationalInstitution;
+        public readonly System.UInt16 Year;
+        public readonly string Info;
+        public readonly string Speciality;
+        public readonly string SpecialtyDirection;
+        public readonly string Specialization;
+        public readonly string Qualification;
+        public readonly string Group;
+
+        /// <summary>
+        /// Строки таблицы
+        /// </summary>
+        public Row TableRow;
+
+        /// <summary>
+        /// Экземпляр строки
+        /// </summary>
+        public class Row
+        {
+            public string FIO;
+            /// <summary>
+            /// Информация за годы
+            /// </summary>
+            public InformationForTheYear[] InformationOverTheYears;
+        }
+
+        /// <summary>
+        /// Информация за 1 год
+        /// </summary>
+        public class InformationForTheYear
+        {
+            public string OrganizationName;
+            public string Position;
+            public string Note;
         }
     }
 }
